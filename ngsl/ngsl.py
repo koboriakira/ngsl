3a47c831ef065ac0e68ca4f8043f188dfe237bac
Akira Kobori <private.beats@gmail.com>
2020-10-01 16:12:14 +0900

    Add methods

--- ngsl/ngsl.py
+++ ngsl/ngsl.py
@@ -1,5 +1,5 @@
-from typing import Optional
-from ngsl.inverted_dictionary import INVERTED_DICTIONARY
+from typing import Optional, List, Set
+from ngsl.inverted_dictionary import INVERTED_DICTIONARY, DICTIONARY
 
 
 def include(word: str) -> bool:
@@ -14,3 +14,20 @@ def get_infinitiv(word: str) -> Optional[str]:
     Return the infinitiv of the word
     """
     return INVERTED_DICTIONARY[word] if include(word) else None
+
+
+def get_infinitiv_list(words: List[str]) -> Set[str]:
+    result: List[str] = []
+    for word in words:
+        infinitiv: Optional[str] = get_infinitiv(word=word)
+        if infinitiv is None:
+            continue
+        result.append(infinitiv)
+    return set(result)
+
+
+def all_infinitiv() -> List[str]:
+    """
+    Return all word that belongs to NGSL.
+    """
+    return DICTIONARY.keys()
